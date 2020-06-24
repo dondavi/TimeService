156aa71bf8defe8fe59afd5395f506c11cd29fe6
David Sanchez <david@dondavi.com>
2020-06-24 11:01:28 -0500

    init

---
+++ TimeService/TimeProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeService
+{
+    public class TimeProcessor : ITimeProcessor
+    {
+        public DateTime CurrentTimeUTC()
+        {
+            return DateTime.UtcNow;
+        }
+    }
+}
