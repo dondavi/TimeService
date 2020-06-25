b0aec16f93df59fc42ee436df10225cc89df0130
David Sanchez <david@dondavi.com>
2020-06-24 20:56:36 -0500

    cc

---
+++ TimeService.Tests/TimeProcessorTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TimeService.Tests
+{
+    public class TimeProcessorTest
+    {
+        [Fact]
+        public void TestCurrentTimeUTC()
+        {
+            // Arrange
+            var processor = new TimeProcessor();
+            var preTestTimeUtc = DateTime.UtcNow;
+
+            // Act
+            var result = processor.CurrentTimeUTC();
+           // var finalresult = result.ToString();
+
+            // Assert time moves forwards 
+            var postTestTimeUtc = DateTime.UtcNow;
+            Assert.True(result >= preTestTimeUtc);
+            Assert.True(result <= postTestTimeUtc);
+        }
+    }
+}
